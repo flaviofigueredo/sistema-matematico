cbfbd66c845d37356c5e915320708562f3bd195d
Flávio Figueredo <flaviofigueredo2828@gmail.com>
2026-09-01 14:18:58 -0300

    feat: finaliza o modulo de funcionalidade matematica

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 // MÓDULO PRINCIPAL : index.js
 // ==========================================
 // O comando 'require ' importa as lógicas que estão nos outros arquivos
-const calculadora = require ('./ calculadora ');
-const bhaskara = require ('./ bhaskara ');
+const calculadora = require ('./calculadora');
+const bhaskara = require ('./bhaskara');
 console.log ("===================================") ;
 console.log ("[ INÍCIO] SISTEMA MATEMÁTICO INICIADO [INÍCIO ]");
 console.log ("===================================\ n");
 // Testando o módulo da calculadora criado pelo colega
 console.log("--- TESTANDO A CALCULADORA ---");
-console.log(`Soma de 10 + 5 = ${ calculadora .somar (10, 5)}`);
-console.log(` Subtracao de 20 - 8 = ${ calculadora .subtrair (20, 8)}`);
-console.log(` Multiplicacao de 4 * 6 = ${ calculadora . multiplicar (4, 6)}`);
-console.log(`Divisao de 15 / 3 = ${ calculadora .dividir (15, 3)}`);
-console.log(`Teste erro divisao = ${ calculadora .dividir (10, 0)}`);
+console.log(`Soma de 10 + 5 = ${ calculadora.somar (10, 5)}`);
+console.log(` Subtracao de 20 - 8 = ${ calculadora.subtrair (20, 8)}`);
+console.log(` Multiplicacao de 4 * 6 = ${ calculadora.multiplicar (4, 6)}`);
+console.log(`Divisao de 15 / 3 = ${ calculadora.dividir (15, 3)}`);
+console.log(`Teste erro divisao = ${ calculadora.dividir (10, 0)}`);
 console.log ("\n--- TESTANDO O BHASKARA ---");
 // Equação base: x2 - 5x + 6 = 0 (Onde a=1, b=-5, c=6. As raízes devem ser 3 e 2)
 const resultado = bhaskara (1, -5, 6);
